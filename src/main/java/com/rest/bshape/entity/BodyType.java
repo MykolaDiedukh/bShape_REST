f355490b53a51cfe814737f4cf8e9cc808dc28b9
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-08-05 18:51:12 +0200

    Added Validation for Business Classes.

--- src/main/java/com/rest/bshape/entity/BodyType.java
+++ src/main/java/com/rest/bshape/entity/BodyType.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Entity
@@ -13,6 +14,8 @@ public class BodyType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
     private String typeOfBody;
 
     @OneToMany(fetch = FetchType.EAGER)
--- src/main/java/com/rest/bshape/entity/Product.java
+++ src/main/java/com/rest/bshape/entity/Product.java
@@ -4,21 +4,33 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
 @Setter
 @Getter
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
+    @Size(min = 3)
     private String name;
+
     private Double gigajoule;
-    private Double alkohol;
+
     private Double calories;
+
+    private Double alcohol;
+
     private Double protein;
+
     private Double fat;
+
     private Double carbohydrates;
 
     @ManyToMany(fetch = FetchType.EAGER)
