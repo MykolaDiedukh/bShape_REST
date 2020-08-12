dbe6e7c6f89a6b2fe11f00b3246642fdea5b7c45
soul <ykrainec1991@gmail.com>
2020-08-12 21:50:20 +0200

    add CrossOrigin

--- src/main/java/com/rest/bshape/repository/BodyTypeRepository.java
+++ src/main/java/com/rest/bshape/repository/BodyTypeRepository.java
@@ -3,7 +3,9 @@ package com.rest.bshape.repository;
 import com.rest.bshape.entity.BodyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin(origins = "http://localhost:4200")
 public interface BodyTypeRepository extends JpaRepository<BodyType, Long> {
 }
--- src/main/java/com/rest/bshape/repository/MealRepository.java
+++ src/main/java/com/rest/bshape/repository/MealRepository.java
@@ -3,7 +3,9 @@ package com.rest.bshape.repository;
 import com.rest.bshape.entity.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin(origins = "http://localhost:4200")
 public interface MealRepository extends JpaRepository<Meal, Long> {
 }
