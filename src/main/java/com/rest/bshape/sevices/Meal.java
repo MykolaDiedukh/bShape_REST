805536822c2d215927712e815fb1952838950cb6
soul <ykrainec1991@gmail.com>
2020-08-15 21:25:26 +0200

    Added to UserHistory 4 fields and services Meal for future calculation

---
+++ src/main/java/com/rest/bshape/sevices/Meal.java
@@ -0,0 +1,11 @@
+package com.rest.bshape.sevices;
+
+public interface Meal {
+
+    public double getAlcohol();
+    public double getCalorie();
+    public double getCarbs();
+    public double getFat();
+    public double getGigajoule();
+    public double getProtein();
+}
