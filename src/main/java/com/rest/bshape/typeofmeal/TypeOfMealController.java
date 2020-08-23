dfc33677ad4a46507b80628d0b87a1e95b7ff9f0
soul <ykrainec1991@gmail.com>
2020-08-23 14:19:22 +0200

    Fixed all methods in TypeOfMealService and TypeOfMealController

--- src/main/java/com/rest/bshape/typeofmeal/TypeOfMealController.java
+++ src/main/java/com/rest/bshape/typeofmeal/TypeOfMealController.java
@@ -1,6 +1,6 @@
 package com.rest.bshape.typeofmeal;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.rest.bshape.exeption.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,31 +11,35 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class TypeOfMealController {
 
-    @Autowired
-    private TypeOfMealService typeOfMealService;
+    private final TypeOfMealService typeOfMealService;
+
+    public TypeOfMealController(TypeOfMealService typeOfMealService) {
+        this.typeOfMealService = typeOfMealService;
+    }
 
     @GetMapping
-    public List<TypeOfMeal> findAll() {
+    public List<TypeOfMealDTO> findAll() {
         return this.typeOfMealService.findAll();
     }
 
     @GetMapping("/{id}")
-    public TypeOfMeal findById(@PathVariable(value = "id") Long id) {
-        return this.typeOfMealService.findById(id);
+    public TypeOfMealDTO findById(@PathVariable(value = "id") Long id) {
+        return typeOfMealService.findById(id).orElseThrow(() -> new ResourceNotFoundException("TypeOfMeal not found with id :" + id));
     }
 
     @PostMapping
-    public TypeOfMeal create(@RequestBody TypeOfMeal typeOfMeal){
-        return this.typeOfMealService.create(typeOfMeal);
+    public TypeOfMealID create(@RequestBody TypeOfMealDTO typeOfMealDTO) {
+        return typeOfMealService.create(typeOfMealDTO).orElseThrow(() -> new ResourceNotFoundException("User not created"));
     }
 
+
     @PutMapping("/{id}")
-    public TypeOfMeal update(@RequestBody TypeOfMeal typeOfMeal, @PathVariable(value = "id") Long id){
-        return this.typeOfMealService.update(typeOfMeal,id);
+    public TypeOfMealDTO update(@RequestBody TypeOfMealDTO metypeOfMealDTOlDTO, @PathVariable("id") Long id) {
+        return typeOfMealService.update(metypeOfMealDTOlDTO, id).orElseThrow(() -> new ResourceNotFoundException("User not found with id :" + id));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<TypeOfMeal> delete(@PathVariable("id") Long id){
-return this.typeOfMealService.delete(id);
+    public ResponseEntity<TypeOfMeal> delete(@PathVariable("id") Long id) {
+        return this.typeOfMealService.delete(id);
     }
 }
--- src/main/java/com/rest/bshape/typeofmeal/TypeOfMealService.java
+++ src/main/java/com/rest/bshape/typeofmeal/TypeOfMealService.java
@@ -1,50 +1,76 @@
 package com.rest.bshape.typeofmeal;
 
-import com.rest.bshape.sevices.GenericService;
-import com.rest.bshape.user.User;
 import com.rest.bshape.exeption.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class TypeOfMealService implements GenericService<TypeOfMeal> {
+public class TypeOfMealService {
 
-    @Autowired
-    private TypeOfMealRepository typeOfMealRepository;
+    private final TypeOfMealRepository typeOfMealRepository;
 
-    @Override
-    public List<TypeOfMeal> findAll() {
-        return this.typeOfMealRepository.findAll();
+    public TypeOfMealService(TypeOfMealRepository typeOfMealRepository) {
+        this.typeOfMealRepository = typeOfMealRepository;
     }
 
-    @Override
-    public Optional<User> findById(Long id) {
-        return this.typeOfMealRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Type of meal not found with id: " + id));
+
+    public List<TypeOfMealDTO> findAll() {
+        List<TypeOfMeal> optionalTypeOfMeal = this.typeOfMealRepository.findAll();
+        return optionalTypeOfMeal.isEmpty() ? Collections.emptyList() : optionalTypeOfMeal.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
-    @Override
-    public TypeOfMeal create(TypeOfMeal typeOfMeal) {
-        return this.typeOfMealRepository.save(typeOfMeal);
+
+    public Optional<TypeOfMealDTO> findById(Long id) {
+        Optional<TypeOfMeal> optionalTypeOfMeal = typeOfMealRepository.findById(id);
+        return optionalTypeOfMeal.isEmpty() ? Optional.empty() : optionalTypeOfMeal.map(this::convertToDTO);
     }
 
-    @Override
-    public TypeOfMeal update(TypeOfMeal typeOfMeal, Long id) {
-        TypeOfMeal existingMeal = this.typeOfMealRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Type of meal was not found, with id: " + id));
-        existingMeal.setTypeMeals(typeOfMeal.getTypeMeals());
-        return this.typeOfMealRepository.save(existingMeal);
+    public Optional<TypeOfMealID> create(TypeOfMealDTO typeOfMealDTO) {
+        TypeOfMeal typeOfMeal = this.convertFromDTO(typeOfMealDTO);
+
+        TypeOfMeal createdTypeOfMeal = typeOfMealRepository.save(typeOfMeal);
+        val typeOfMealID = new TypeOfMealID(createdTypeOfMeal.getId());
+        return Optional.of(typeOfMealID);
     }
 
-    @Override
+    public Optional<TypeOfMealDTO> update(TypeOfMealDTO typeOfMealDTO, Long id) {
+        TypeOfMeal typeOfMeal = this.convertFromDTO(typeOfMealDTO);
+
+        Optional<TypeOfMeal> typeOfMealById = typeOfMealRepository.findById(id);
+        if (typeOfMealById.isEmpty()) {
+            return Optional.empty();
+        }
+        TypeOfMeal existingTypeOfMeal = typeOfMealById.get();
+        existingTypeOfMeal.setTypeMeals(typeOfMeal.getTypeMeals());
+        return Optional.of(this.convertToDTO(typeOfMealRepository.save(existingTypeOfMeal)));
+    }
+
     public ResponseEntity<TypeOfMeal> delete(Long id) {
         TypeOfMeal existingMeal = this.typeOfMealRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Type of meal was not found, with id: " + id));
         this.typeOfMealRepository.delete(existingMeal);
         return ResponseEntity.ok().build();
+    }
+
+    private TypeOfMealDTO convertToDTO(TypeOfMeal typeOfMeal) {
+        return TypeOfMealDTO.builder()
+                .id(typeOfMeal.getId())
+                .typeMeals(typeOfMeal.getTypeMeals())
+                .build();
+    }
+
+    private TypeOfMeal convertFromDTO(TypeOfMealDTO typeOfMealDTO) {
+        return TypeOfMeal.builder()
+                .id(typeOfMealDTO.getId())
+                .typeMeals(typeOfMealDTO.getTypeMeals())
+                .build();
     }
 }
