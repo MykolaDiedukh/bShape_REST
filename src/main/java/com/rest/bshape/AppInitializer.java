ed283b86a4fa95a77853c5a83020f20391b508a3
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-08-31 20:17:48 +0200

    Added Swagger to pomxml, and included Swagger in AppInitializer.

--- src/main/java/com/rest/bshape/AppInitializer.java
+++ src/main/java/com/rest/bshape/AppInitializer.java
@@ -2,14 +2,18 @@ package com.rest.bshape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-
+@EnableSwagger2
 public class AppInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppInitializer.class, args);
 	}
+
+
+	//http://localhost:8092/swagger-ui.html wchodzę pod ten adres i widzę controlery i mogę testować rest api
 
 }
