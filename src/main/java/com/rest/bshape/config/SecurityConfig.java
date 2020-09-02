af0db871a3055bf0331ff24a80ee62eadccbdee1
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-02 17:22:39 +0200

    Created Class SecurityConfig

---
+++ src/main/java/com/rest/bshape/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.rest.bshape.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // włącza działanie adnotacji preautorajz
+public class SecurityConfig {
+
+
+
+}
