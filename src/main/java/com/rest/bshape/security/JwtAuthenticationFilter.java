2adc4c69d22d0cd28113bbef5e1bfe5ebc751970
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-04 17:33:41 +0200

    Created Claims with information about lifeCycle of token, and his informations we r going to put in future.

--- src/main/java/com/rest/bshape/security/JwtAuthenticationFilter.java
+++ src/main/java/com/rest/bshape/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.impl.DefaultClaims;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -12,8 +13,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.Principal;
 import java.util.Date;
+
+import static java.util.stream.Collectors.joining;
 
 // glasa do generowania tokenów po poprawnym zalogowanu
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -29,11 +31,12 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
 
         Claims claims = new DefaultClaims()
-                .setSubject(((UserDetails)authResult.getPrincipal()).getUsername()) // castuje do interfejsu UserDetails aby pobrać nazwę uzytkownika ktory poprawnie sie zalogowal
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24 ));  // jak długo token jest wazny - token jest ważny cały dzień
+                .setSubject(((UserDetails) authResult.getPrincipal()).getUsername()) // castuje do interfejsu UserDetails aby pobrać nazwę uzytkownika ktory poprawnie sie zalogowal
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24));  // jak długo token jest wazny - token jest ważny cały dzień
 
 
-
-
+        claims.put("authorities", authResult.getAuthorities().stream()            /// pobrałem liste simpleAuhtority
+                .map(GrantedAuthority::getAuthority) // metoda referencyjna
+                .collect(joining(","))); // mapuje, pobieram auuthority i colectuje łącząc przy pomocy joning.
     }
 }
