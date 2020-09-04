8ceaaa58503627978b395df1ba5933ab6adbd51c
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-04 17:19:38 +0200

    Added Claims class to succesfulAuth and Castedd it to userDetails.

--- src/main/java/com/rest/bshape/security/JwtAuthenticationFilter.java
+++ src/main/java/com/rest/bshape/security/JwtAuthenticationFilter.java
@@ -1,7 +1,10 @@
 package com.rest.bshape.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.impl.DefaultClaims;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +12,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Principal;
+import java.util.Date;
 
 // glasa do generowania tokenów po poprawnym zalogowanu
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -22,6 +27,13 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     // bedzie generowac token po poprawnym zalogowaniu
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+
+        Claims claims = new DefaultClaims()
+                .setSubject(((UserDetails)authResult.getPrincipal()).getUsername()) // castuje do interfejsu UserDetails aby pobrać nazwę uzytkownika ktory poprawnie sie zalogowal
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24 ));  // jak długo token jest wazny - token jest ważny cały dzień
+
+
+
+
     }
 }
