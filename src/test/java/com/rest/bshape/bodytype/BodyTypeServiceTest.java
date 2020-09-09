75b20c6eab4434bfcde48884588804d5fbae9122
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-09 18:25:21 +0200

    Created Mock of object - bodyTypeRepository and Injected mock of BodyTYpeServicie wich im going to test

---
+++ src/test/java/com/rest/bshape/bodytype/BodyTypeServiceTest.java
@@ -0,0 +1,18 @@
+package com.rest.bshape.bodytype;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+class BodyTypeServiceTest {
+
+    @Mock
+    private BodyTypeRepository bodyTypeRepository;
+
+    @InjectMocks
+    private BodyTypeService bodyTypeService;
+
+    @Test
+    void update() {
+    }
+}
