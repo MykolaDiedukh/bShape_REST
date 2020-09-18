5c35984115cc3c964e7c4b05c777f3f108778ef7
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-09-18 17:55:25 +0200

    Added SpringContext for Integration-tests

---
+++ src/test/java/com/rest/bshape/bodytype/BodyTypeControllerTest.java
@@ -0,0 +1,17 @@
+package com.rest.bshape.bodytype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-local.properties")
+public class BodyTypeControllerTest {
+
+    @Test
+    public void doSthg(){
+
+    }
+
+
+}
